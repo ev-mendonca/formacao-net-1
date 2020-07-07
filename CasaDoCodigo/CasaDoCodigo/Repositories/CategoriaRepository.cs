bd30c713809497125fe107eb8452442b76f6648f
Everton Mendonça <evertonavilamendonca@gmail.com>
2020-07-06 21:40:17 -0300

    Commit inicial da estrutura contendo a funcionalidade 1 proposta

---
+++ CasaDoCodigo/CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,53 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Categoria GetCategoria(int id);
+        Task<Categoria> SaveCategoria(string nome); 
+    }
+
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+        
+        public CategoriaRepository(ApplicationContext contexto,
+            IHttpContextAccessor contextAccessor) : base(contexto)
+        {
+            this.contextAccessor = contextAccessor;
+        }
+        public Categoria GetCategoria(int id)
+        {
+            return dbSet
+                .Include(c => c.Produtos)
+                .Where(c => c.Id == id)
+                .SingleOrDefault();
+        }
+
+        public async Task<Categoria> SaveCategoria(string nome)
+        {
+            Categoria categoria = GetCategoria(nome);
+            if(categoria == null)
+            {
+                categoria = new Categoria(nome);
+                dbSet.Add(categoria);
+                await contexto.SaveChangesAsync();
+                
+            }
+            return categoria;
+        }
+
+        private Categoria GetCategoria(string nome)
+        {
+            return dbSet.Where(c => c.Nome == nome).SingleOrDefault();
+        }
+    }
+}
---
+++ CasaDoCodigo/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,45 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+    {
+        private readonly ICategoriaRepository _categoriaRepository; 
+        public ProdutoRepository(ApplicationContext contexto, ICategoriaRepository categoriaRepository) : base(contexto)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+            return dbSet.ToList();
+        }
+
+        public async Task SaveProdutos(List<Livro> livros)
+        {
+            foreach (var livro in livros)
+            {
+                if (!dbSet.Where(p => p.Codigo == livro.Codigo).Any())
+                {
+                    Categoria categoria = await _categoriaRepository.SaveCategoria(livro.Categoria);
+                    dbSet.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco, categoria));
+                }
+            }
+            await contexto.SaveChangesAsync();
+        }
+    }
+
+    public class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Subcategoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
